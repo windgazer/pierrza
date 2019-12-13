419c0b02cfda6f60da3a18dee2caedc1c6c95522
PGaujac <degaujacp@gmail.com>
2019-12-13 15:43:42 +0100

    removed materialize, added material ui

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,9 @@
 
 //Module import
 import React, { Component } from 'react';
-import { Carousel, Button, Icon } from 'react-materialize';
-import M from 'materialize-css/dist/js/materialize.min.js';
 
 //Style import
 import './Home.css';
-import 'materialize-css/dist/css/materialize.min.css';
 
 class Home extends Component {
 	constructor(props) {
@@ -16,49 +13,10 @@ class Home extends Component {
 		this.state = {};
 	}
 
-	componentDidMount() {
-		this._auto();
-	}
-
-	_auto = () => {
-		setInterval(() => {
-			const element = M.Carousel.getInstance(
-				document.querySelector('.images')
-			);
-			element.next();
-		}, 2000);
-	};
-
 	render() {
 		return (
 			<div className="homeContainer">
-				<div className="carousel-container">
-					<Carousel
-						images={[
-							'/img/pizza1.jpg',
-							'/img/pizza2.jpg',
-							'/img/pizza3.jpg',
-							'/img/pizza4.jpg',
-							'/img/pizza5.jpg'
-						]}
-						options={{
-							fullWidth: true,
-							indicators: false
-						}}
-						className="images"
-					/>
-				</div>
-				<Button
-					large
-					node="a"
-					style={{
-						marginRight: '5px'
-					}}
-					waves="light"
-				>
-					Button
-					<Icon left>cloud</Icon>
-				</Button>
+				<div className="carousel-container"></div>
 			</div>
 		);
 	}
